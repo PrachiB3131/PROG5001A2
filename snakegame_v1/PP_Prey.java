5b438dad9d0eb573c5bf5a6503344133f0b21ddf
Prachi <p.pundir.10@student.scu.edu.au>
2021-04-25 02:27:58 +0800

    Prey Class Updation
    Updated class and constructor name

--- snakegame_v1/PP_Prey.java
+++ snakegame_v1/PP_Prey.java
@@ -4,11 +4,11 @@
  * @author 
  * @version
  */
-public class Prey {
+public class PP_Prey {
     /**
      * Constructor for objects of class Prey
      */
-    public Prey()
+    public PP_Prey()
     {
     }    
 }
