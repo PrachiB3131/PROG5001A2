fcc80906c6431bf1f03858033245172eca82e49f
Prachi <p.pundir.10@student.scu.edu.au>
2021-05-09 19:17:42 +0800

    Snake Class Updation
    1. Added snake head image
    2. Added snake body Image
    3. Started for loop for the body of snake

--- snakegame_v1/PP_Snake.java
+++ snakegame_v1/PP_Snake.java
@@ -1,45 +1,58 @@
 /**
- * Write a description of class Snake here.
+ * Class PP_Snake represents the snake object.
  *
  * @author Prachi(23820430)
  * @version (01/05/2021)
  */
-abstract class PP_Snake
-{
-    /**
-     * Constructor for objects of class Snake
-     */
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+public class PP_Snake extends JPanel {
+    Image head;
+    Image body;
     public PP_Snake() {
-        // initialise instance variables        
+        head= new ImageIcon("Images/Head.png").getImage();
+        body= new ImageIcon("Images/Body.png").getImage();
+        Dimension dimension = new Dimension(50,50);
+        setPreferredSize(dimension);
     }
 
-    /**
-     * Method move
-     * controls the movement of snake (up,down, right, left)
-     * user controls this movement by pressing keyboard keys
-     */
-    public abstract void move();
-    {
-        // put your code here
-    }
-    /**
-     * Method CheckPrey
-     * snake finds prey to eat it
-     * it checks  x and y coordinates of the apple till they matches the
-     * x and y coordinates of the snake head
-     */
-    public void CheckPrey()
-    {
-        // put your code here
-    }
-    /**
-     * Method checkCollision
-     * it checks if the snake collides with the wall or itself 
-     */
-    public void checkCollision()
-    {
-        // put your code here
+    public void paintComponent(Graphics g) {
+        g.drawImage(head, 20, 20, null);
+        repaint();
+
+        int x_axis = 100;
+        int y_axis = 150;
+
+        for(int i=0; i<3; i++)
+        {
+            g.drawImage(body, 100+(i*250), 100, null);
+            //repaint();
+            //y_axis = y_axis + 30;
+        }
+
+        for(int j=0; j<4; j++)
+        { 
+            g.drawImage(body,900,100+(j*250), null);
+            //repaint();
+            //x_axis = x_axis + 30;
+        }
+        repaint();
+        g.drawImage(body, 100, 100, null);
     }
 
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Image Snake");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PP_Snake panel = new PP_Snake();
 
-}
+        frame.add(panel);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}  
