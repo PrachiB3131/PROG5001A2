5e7726a7bd4d587ca11caed9065f2e402414a77d
Prachi <p.pundir.10@student.scu.edu.au>
2021-05-09 19:21:02 +0800

    LoginForm Class Updation
    1. Authentication for the game GUI i.e. after Login, object of Snake Game class is called and GUI is opened on login.

--- snakegame_v1/PP_LoginForm.java
+++ snakegame_v1/PP_LoginForm.java
@@ -13,7 +13,8 @@ import java.awt.event.*;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
- 
+import java.lang.String;
+
 /**
  * This program demonstrates how to use JFrame and LayoutManager.
  * @author Vinh Bui
@@ -26,7 +27,6 @@ public class PP_LoginForm extends JFrame implements ActionListener {
     private JPasswordField fieldPassword;
     private JButton buttonLogin;
     private PP_PlayerList playerList;
- 
     public PP_LoginForm() {
         super("Login Form");
         labelUsername = new JLabel("Enter username: ");
@@ -34,47 +34,47 @@ public class PP_LoginForm extends JFrame implements ActionListener {
         textUsername = new JTextField(20);
         fieldPassword = new JPasswordField(20);
         buttonLogin = new JButton("Login");
-        
+
         // create a new panel with GridBagLayout manager
         JPanel panelLogin = new JPanel(new GridBagLayout());
-        
+
         //use contrains to control the gridbaglayout
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.WEST;
         constraints.insets = new Insets(10, 10, 10, 10);
-         
+
         // add components to the panel
         constraints.gridx = 0;
         constraints.gridy = 0;     
         panelLogin.add(labelUsername, constraints);
- 
+
         constraints.gridx = 1;
         panelLogin.add(textUsername, constraints);
-         
+
         constraints.gridx = 0;
         constraints.gridy = 1;     
         panelLogin.add(labelPassword, constraints);
-         
+
         constraints.gridx = 1;
         panelLogin.add(fieldPassword, constraints);
-         
+
         constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.gridwidth = 2;
         constraints.anchor = GridBagConstraints.CENTER;
         panelLogin.add(buttonLogin, constraints);
-                  
+
         // add the panel to this frame
         add(panelLogin);
-         
+
         pack();
-        
+
         //make the form apprear in the screen centre
         setLocationRelativeTo(null);
-        
+
         //add ActionListener to the button
         buttonLogin.addActionListener(this);
-        
+
         //instantiate the playerList
         playerList = new PP_PlayerList();
         try {
@@ -83,24 +83,29 @@ public class PP_LoginForm extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(this, "File Not Found");
         }
     }
-    
+
     public void actionPerformed(ActionEvent e) {
         String username = textUsername.getText();
         String password = fieldPassword.getText();
         if (playerList.matchPlayer(username, password)) {
             JOptionPane.showMessageDialog(this, username + ": login successfully");
+            // Create object for the class to run it
+
+            PP_SnakeGame game = new PP_SnakeGame("snake");
+            game.setVisible(true);
+
         } else {
             JOptionPane.showMessageDialog(this, "wrong username or password");
         }
 
     }
-    
+
     private void readPlayerFromFile(String fileName) throws FileNotFoundException{
         File file = new File(fileName);
-     
+
         //Creating Scanner instnace to read File in Java
         Scanner scnr = new Scanner(file);
-     
+
         //Reading each line of file using Scanner class
         while(scnr.hasNextLine()){
             String line = scnr.nextLine();
@@ -111,13 +116,13 @@ public class PP_LoginForm extends JFrame implements ActionListener {
             //JOptionPane.showMessageDialog(this, username + ": " + password);
         }      
     }
-    
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new PP_LoginForm().setVisible(true);
-            }
-        });
+                @Override
+                public void run() {
+                    new PP_LoginForm().setVisible(true);
+                }
+            });
     }
 }
