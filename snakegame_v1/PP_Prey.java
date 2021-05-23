cc94522d6176dbe0d4e6703a888eda8de50fa7e4
Prachi <p.pundir.10@student.scu.edu.au>
2021-05-23 15:38:32 +0800

    PP_Prey Class Updation
    1. Declared an image object and called the prey image with that object.
    2. Implemented the random position mechanism for the random allocation of prey

--- snakegame_v1/PP_Prey.java
+++ snakegame_v1/PP_Prey.java
@@ -12,29 +12,45 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import java.awt.EventQueue;
 
-public class PP_Prey extends JPanel {
-    Image img;
-    public PP_Prey(String image_path, int width, int height) {
-        img = new ImageIcon(image_path).getImage();
+public class PP_Prey extends JFrame {
+    public Image img;
+    public int preyx;
+    public int preyy;
+    public PP_Prey() {
+        img = new ImageIcon("Images/Prey.png").getImage();
         //img = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
-        Dimension size = new Dimension(width, height);
+        Dimension size = new Dimension(9, 9);
         setPreferredSize(size);
     }
 
-    public void paintComponent(Graphics g) {
-        g.drawImage(img, 0, 0, null);
-        repaint();
+    /**
+     * Method checkPrey
+     * 
+     */
+    public void checkPrey(int x, int y , int RAND_POSITION, int PREY_SIZE)
+    {
+        if ((x == preyx) && (y == preyy)) {
+
+            PP_GameBoard.snakebody++;
+            randomposition(RAND_POSITION,PREY_SIZE);
+        } 
     }
 
-    public static void main(String args[]) {
-        JFrame frame = new JFrame("Image demo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        PP_Prey panel = new PP_Prey("Images/smiley.jpg", 67, 64);
+    /**
+     * Method randomposition
+     *
+     * It allocates the random position to prey
+     */
+    public void randomposition(int RAND_POSITION, int PREY_SIZE)
+    {
+        int r = (int) (Math.random() * RAND_POSITION);
+        preyx= ((r * PREY_SIZE));
 
-        frame.add(panel);
-        frame.setSize(300, 200);
-        frame.setVisible(true);
+        r = (int) (Math.random() * RAND_POSITION);
+        preyy = ((r * PREY_SIZE));    
+
     }
+
 }
-
