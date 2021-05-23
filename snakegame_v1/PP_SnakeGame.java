d5c1cc760eb0df8642e2a04a45acc7fb131ce4c3
Prachi <p.pundir.10@student.scu.edu.au>
2021-05-23 15:35:46 +0800

    PP_SnakeGame Class Updation
    1. Implemented a GUI with GridBagLayout in a JFrame
    2. Added the actionperformed on click to play button
    3. Made an object "newgame" which will call the PP_GameBoard class on clicking the ClickToPlay button

--- snakegame_v1/PP_SnakeGame.java
+++ snakegame_v1/PP_SnakeGame.java
@@ -6,6 +6,14 @@
  */
 import java.awt.Color;
 import java.awt.EventQueue;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
+import javax.swing.border.LineBorder;
+import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -13,52 +21,47 @@ import java.awt.Image;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.LayoutStyle;
-import javax.swing.border.LineBorder;
-import java.awt.Insets;
-import javax.swing.GroupLayout;
-import javax.swing.ImageIcon;
+import java.awt.Dimension;
 
-public class PP_SnakeGame extends JFrame {
+public class PP_SnakeGame extends JFrame implements ActionListener {
     private JLabel GameOver;
     private JButton ClickToPlay;
     private JLabel TopPlayersScore;   
     private JLabel CurrentPlayerScore;
-    private Image Snake;
-    private JLabel SnakeLabel;
-    private JLabel Name;
     private JButton Quit;
     private Graphics g;
     private JLabel MySnakeGame; 
+    private Image Snake;
+    private JLabel SnakeLabel;
+    private JLabel Name;
 
     public PP_SnakeGame(String SnakeGame) {  
         super(SnakeGame);
 
         GameOver = new JLabel("GAME OVER");
         ClickToPlay = new JButton("CLICK TO PLAY");   
-        TopPlayersScore = new JLabel("TOP PLAYER'S SCORE");   
-        CurrentPlayerScore = new JLabel("CURRENT PLAYER SCORE"); 
-        Name = new JLabel("PROG5001:2021 PRACHI");
+        TopPlayersScore = new JLabel("TOP PLAYER'S SCORE");  
         SnakeLabel = new JLabel();
         Quit = new JButton("QUIT");
         Quit.setBackground(Color.BLUE);
-        MySnakeGame= new JLabel("MY SNAKE GAME (C)");
+        MySnakeGame= new JLabel("MY SNAKE GAME ©");
+        CurrentPlayerScore = new JLabel("CURRENT PLAYER SCORE"); 
+        Name = new JLabel("PROG5001:2021 PRACHI");
 
         //NORTH COMPONENTS
-        JPanel topPanel = new JPanel(new GridBagLayout());
-        topPanel.setBackground(Color.BLUE);
-        topPanel.setBounds(10,10,1500,40);
-        GridBagConstraints constraints1= new GridBagConstraints();
-        constraints1.anchor = GridBagConstraints.NORTH;
-        constraints1.insets = new Insets(10, 10, 10, 10);
-        topPanel.add(MySnakeGame,constraints1);
+        JPanel NPanel = new JPanel(new GridBagLayout());
+        NPanel.setBackground(Color.BLUE);
+        NPanel.setBounds(10,10,1500,40);
+        GridBagConstraints northconstraints1= new GridBagConstraints();
+        northconstraints1.anchor = GridBagConstraints.NORTH;
+        northconstraints1.insets = new Insets(10, 10, 10, 10);
+        NPanel.add(MySnakeGame,northconstraints1);
         MySnakeGame.setForeground(Color.white);
 
         //WEST COMPONENTS 
-        JPanel leftPanel = new JPanel(new GridBagLayout());
-        leftPanel.setBackground(Color.black);
-        leftPanel.setBounds(10,70,550,700);
+        JPanel WPanel = new JPanel(new GridBagLayout());
+        WPanel.setBackground(Color.black);
+        WPanel.setBounds(10,70,550,700);
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.WEST;
         constraints.insets = new Insets(10, 10, 10, 10);
@@ -67,16 +70,17 @@ public class PP_SnakeGame extends JFrame {
         constraints.gridx = 0;
         constraints.gridy = 0;     
         GameOver.setForeground(Color.white);
-        leftPanel.add(GameOver,constraints);
+        WPanel.add(GameOver,constraints);
 
         //CLICK TO PLAY BUTTON
         constraints.gridx = 0;
-        constraints.gridy = 1;  
-        leftPanel.add(ClickToPlay, constraints);
+        constraints.gridy = 1; 
+        ClickToPlay.addActionListener(this);
+        WPanel.add(ClickToPlay, constraints);
 
         //EAST COMPONENTS
-        JPanel rightPanel = new JPanel(new GridBagLayout());
-        rightPanel.setBounds(580,70 ,550,700);
+        JPanel EPanel = new JPanel(new GridBagLayout());
+        EPanel.setBounds(580,70 ,550,700);
         GridBagConstraints constraint = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.EAST;
         constraints.insets = new Insets(10, 10, 10, 10);
@@ -84,48 +88,53 @@ public class PP_SnakeGame extends JFrame {
         //TOP PLAYER'S SCORE LABEL
         constraints.gridx = 0;
         constraints.gridy = 0;     
-        rightPanel.add(TopPlayersScore, constraints);
+        EPanel.add(TopPlayersScore, constraints);
         TopPlayersScore.setBorder(new LineBorder(Color.blue,1));
 
         //CURRENT PLAYER SCORE LABEL
         constraints.gridx = 0;  
         constraints.gridy = 1;
-        rightPanel.add(CurrentPlayerScore, constraints);
+        EPanel.add(CurrentPlayerScore, constraints);
         CurrentPlayerScore.setBorder(new LineBorder(Color.blue,1));
 
         //NAME LABEL
         constraints.gridx = 0;  
         constraints.gridy = 3;
-        rightPanel.add(Name, constraints);
+        EPanel.add(Name, constraints);
         Name.setBorder(new LineBorder(Color.blue,1));
 
         //SNAKE IMAGE LABEL
         constraints.gridx = 0;  
         constraints.gridy = 2;
         SnakeLabel.setIcon(new ImageIcon("Images/BigSnake.jpg"));
-        rightPanel.add(SnakeLabel,constraints);
+        EPanel.add(SnakeLabel,constraints);
 
         //QUIT BUTTON
         constraints.gridx = 0;  
         constraints.gridy = 4;
-        rightPanel.add(Quit, constraints);
+        EPanel.add(Quit, constraints);
 
         //Adding all the components to the panel
-        add(topPanel);
-        add(leftPanel);
-        add(rightPanel);
+        add(NPanel);
+        add(WPanel);
+        add(EPanel);
+
+        setPreferredSize(new Dimension(1500, 1500));
         setLocationRelativeTo(null);
         setVisible(true);
         pack();
 
-        
     }
 
-    public static void main(String[] args) {        
-        //        EventQueue.invokeLater(() -> {
-        //create the game with a game title as follow: The Snake Game (C) Your_Name
-        JFrame sgame = new PP_SnakeGame("snake");
-        sgame.setVisible(true);
-        //        });
+    public void actionPerformed(ActionEvent e){
+        JFrame newgame = new JFrame();
+        newgame.add(new PP_GameBoard());
+        newgame.setResizable(false);
+        newgame.pack();
+        newgame.setLocationRelativeTo(null);
+        newgame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newgame.setTitle("Snake");
+        newgame.setVisible(true);
+
     }
 }
