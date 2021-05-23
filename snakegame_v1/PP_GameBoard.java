d16241cd3701966b01155e5238d49be360ed8de3
Prachi <p.pundir.10@student.scu.edu.au>
2021-05-23 15:29:41 +0800

    PP_Gameboard Class Updation
    1. This class contains all the code for Game Board panel.
    2.  After the game is over it is redirected to the PP_SnakeGame class where JFrame is opened.

--- snakegame_v1/PP_GameBoard.java
+++ snakegame_v1/PP_GameBoard.java
@@ -1,64 +1,262 @@
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import javax.swing.JPanel;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 
-abstract public class PP_GameBoard extends JPanel {    
-    int xcells = 30;    
-    int ycells = 30;
-    int szcell = 10;        
+public class PP_GameBoard extends JPanel implements ActionListener {
+
+    private final int GAMEBOARD_WIDTH = 600; 
+    private final int GAMEBOARD_HEIGHT = 600;
+    private final int PREY_SIZE = 10;  
+    private final int ALL_PREY = 900; 
+    private final int RAND_POSITION = 29; 
+    private final int DELAY = 140; 
+    private final int x[] = new int[ALL_PREY]; 
+    private final int y[] = new int[ALL_PREY]; 
+    public static int snakebody;
+
+    private boolean leftMovement = false; 
+    private boolean rightMovement = true; 
+    private boolean upMovement = false;
+    private boolean downMovement = false; 
+    private boolean Gameinprogress = true;
+
+    private PP_Prey p_prey ;
+    private PP_Snake p_snake; 
+    private Timer time; 
+    private Image body; 
+    private Image img; 
+    private Image head; 
 
     public PP_GameBoard() {
-        setPreferredSize(new Dimension(xcells * szcell, ycells * szcell));        
-        setBackground(Color.black);
-        setFocusable(true);        
+
+        p_snake = new PP_Snake();
+        p_prey = new PP_Prey();
+
+        initialiseBoard();
     }
 
+    // gameboard
+    /**
+     * Method initialiseBoard
+     * Initialising the board
+     */
+    private void initialiseBoard() {
+
+        addKeyListener(new KAdapter());
+        setBackground(Color.black);
+        setFocusable(true);
+
+        setPreferredSize(new Dimension(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT));
+        loadingImages();
+        initialiseGame();
+    }
+
+    /**
+     * Method loadingImages
+     * Loading the images from their declared image object 
+     */
+    private void loadingImages() {
+
+        body = p_snake.body;
+        img = p_prey.img;
+        head = p_snake.head;
+    }
+
+    /**
+     * Method initialiseGame
+     * Initialising the game with the moving snake and randomly located prey
+     */
+    private void initialiseGame() {
+
+        snakebody = 3;
+
+        for (int n = 0; n < snakebody; n++) {
+            x[n] = 50 - n * 10;
+            y[n] = 50;
+        }
+
+        p_prey.randomposition(RAND_POSITION,PREY_SIZE);
+        time = new Timer(DELAY, this);
+        time.start();
+    }
+
+    // gameboard
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        doDrawing(g);
+
+    }
+    // gameboard
+    private void doDrawing(Graphics g) {
+
+        if (Gameinprogress) {
+
+            g.drawImage(img,p_prey.preyx,p_prey.preyy, this);
+
+            for (int n = 0; n < snakebody; n++) {
+                if (n == 0) {
+                    g.drawImage(head, x[n], y[n], this);
+                } else {
+                    g.drawImage(body, x[n], y[n], this);
+                }
+            }
+
+            Toolkit.getDefaultToolkit().sync();
+
+        } else {
+
+            gameOver(g);
+            setVisible(false);
+        }        
     }
 
     /**
-     * Method setVisible
-     * For setting the visibility as true or false
-     * by passing the parameter as true or false
+     * Method gameOver
+     * It is drawing the "Game Over" String message and calling the PP_SnakeGame class with object "game" 
+     * @param g A parameter of graphics
      */
-    public abstract void setVisible();
-    {
-        // put your code here
+    private void gameOver(Graphics g) {
+
+        String msg = "Game Over";
+        Font small = new Font("Serif", Font.BOLD, 16);
+        FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.red);
+        g.setFont(small);
+        g.drawString(msg, (GAMEBOARD_WIDTH - metr.stringWidth(msg)) / 2, GAMEBOARD_HEIGHT / 2);
+
+        PP_SnakeGame game = new PP_SnakeGame("Snake");
+        game.setVisible(true);
     }
 
     /**
-     *Method setResizable
-     *This method is used to resize the board
-     * by clicking on the edge of the window and dragging it.
+     * Method movesnake
+     * Movement of the snake 
      */
-    public void setResizable()
-    {
-        // put your code here
+    private void movesnake() {
+
+        for (int n = snakebody; n > 0;  n--) {
+            x[n] = x[(n - 1)];
+            y[n] = y[(n - 1)];
+        }
+
+        if (leftMovement) {
+            x[0] -= PREY_SIZE;
+        }
+
+        if (rightMovement) {
+            x[0] += PREY_SIZE;
+        }
+
+        if (upMovement) {
+            y[0] -= PREY_SIZE;
+        }
+
+        if (downMovement) {
+            y[0] += PREY_SIZE;
+        }
     }
 
     /**
-     * Method keyPressed
-     * user controls the movement of snake by pressing the keyboard arrow keys
-     * up, down, right, left
+     * Method CheckCollision
+     * Checking the collision if snake head is collided with its own body or the board wall.
      */
-    public void keyPressed(KeyEvent e)
-    {
-        // put your code here
-    }
-    /**
-     * Method windowClosing
-     * it closes the window when the window close button is clicked
-     * @param WindowEventE
-     */
-    public void windowClosing(WindowEvent E)
-    {
-        // put your code here
+    private void CheckCollision() {
+
+        for (int n = snakebody; n > 0; n--) {
+
+            if ((n > 4) && (x[0] == x[n]) && (y[0] == y[n])) {
+                Gameinprogress = false;
+            }
+        }
+
+        if (y[0] >= GAMEBOARD_HEIGHT) {
+            Gameinprogress = false;
+        }
+
+        if (y[0] < 0) {
+            Gameinprogress = false;
+        }
+
+        if (x[0] >= GAMEBOARD_WIDTH) {
+            Gameinprogress = false;
+        }
+
+        if (x[0] < 0) {
+            Gameinprogress = false;
+        }
+
+        if (!Gameinprogress) {
+            time.stop();
+        }
     }
 
+    // gameboard
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (Gameinprogress) {
+
+            p_prey.checkPrey(x[0],y[0],RAND_POSITION,PREY_SIZE);
+            CheckCollision();
+            movesnake();
+        }
+
+        repaint();
+    }
+
+    private class KAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightMovement)) {
+                leftMovement = true;
+                upMovement = false;
+                downMovement = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftMovement)) {
+                rightMovement = true;
+                upMovement = false;
+                downMovement = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downMovement)) {
+                upMovement = true;
+                rightMovement = false;
+                leftMovement = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upMovement)) {
+                downMovement = true;
+                rightMovement = false;
+                leftMovement = false;
+            }
+            if (key == KeyEvent.VK_SPACE ){
+                Gameinprogress = !Gameinprogress;
+            }
+
+            if (key == KeyEvent.VK_SPACE && !Gameinprogress) {
+                time.stop();
+            }
+            else if (key == KeyEvent.VK_SPACE && Gameinprogress ){
+                time.start();
+            }
+        }
+    }
 }
